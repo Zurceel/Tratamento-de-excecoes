658764f9905c7291533099c0b0c051d6f03f175d
Gabriel da Cruz Silva <gabrielcruzsil@gmail.com>
2023-10-26 13:50:10 -0300

    Dando andamento no curso

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pessoas = ["Gabriel", "Geovanna"]
+
+    print(pessoas)
+
+    pessoas.insert(0, "Gael")
+
+    print(pessoas)
+
+    pessoas.remove("Gabriel")
+
+    print(pessoas)
+
+    pessoas.sort()
+    print(pessoas)
+
+    quantidade = pessoas.count("Geovanna")
+    print(quantidade)
+
+    pessoas.clear()
+    print(pessoas)
+
+
+
+
+
+
